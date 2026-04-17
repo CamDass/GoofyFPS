e4b2a75b76172793f207cc5a57158ab11da8c0fb
ilianynov <ilian.briki@ynov.com>
2026-04-17 12:26:34 +0200

    texturesniper

--- Program.cs
+++ Program.cs
@@ -317,23 +317,10 @@ class Program
 
         Raylib.BeginMode3D(weaponCamera);
 
-            Vector3 weaponPos = new Vector3(0.4f, -0.4f, 1.2f);
+            Vector3 weaponPos = new Vector3(0.5f, -0.4f, 1.2f);
             Raylib.DrawModel(sniper, weaponPos, 1.0f, Color.White);
         Raylib.EndMode3D();
         
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
