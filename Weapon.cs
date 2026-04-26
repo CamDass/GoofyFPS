80ae59750efc7ce853812f2297b9e133a1474dc1
ilianynov <ilian.briki@ynov.com>
2026-04-26 22:08:15 +0200

    modif model karambit + ajout Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Raylib_cs;
+using System.Numerics;
+
+public class Weapon
+{
+    string name;
+    int damage;
+    int range;
+    float fireRate;
+}
+
+
+
+
+
